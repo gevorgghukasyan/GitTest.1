26a52b4bd3bfb0a92ac08138b8ababb5386307f9
igevorg <igevorg@outlook.com>
2025-02-13 19:03:51 +0400

    added print message

--- GitTest.1/Program.cs
+++ GitTest.1/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GitTest._1
 {
@@ -10,7 +6,9 @@ namespace GitTest._1
 	{
 		static void Main(string[] args)
 		{
+			int num = int.Parse(Console.ReadLine());
 
+			Console.WriteLine("C#");
 		}
 	}
 }
