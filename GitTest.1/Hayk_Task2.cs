04d231c7154614bd548bbcbb597b5f6cb43f3928
Hayk-Grigoryan-92 <hayk.grigoryan992@gmail.com>
2025-02-14 20:07:12 +0400

    Hayk Task3 is done

---
+++ GitTest.1/Hayk_Task2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitTest._1
+{
+    internal class Hayk_Task2
+    {
+        public void Task2()
+        {
+            Console.WriteLine("Task 2");
+        }
+    }
+}
---
+++ GitTest.1/Hayk_Task3.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitTest._1
+{
+    internal class Hayk_Task3
+    {
+        public void Task3()
+        {
+            Console.WriteLine("Task 3");
+        }
+    }
+}
