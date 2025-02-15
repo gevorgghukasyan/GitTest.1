90e25cc4b9a58d6271c409335e4faebe5c5019dd
VahePoghosian <vahe.poghosyan5@edu.ysu.am>
2025-02-15 18:15:16 +0400

    Task Vahe 1 commit

---
+++ GitTest.1/Vahe.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitTest._1
+{
+    internal class Vahe
+    {
+        public static void Show()
+        {
+            Console.WriteLine("Vahe1");
+        }
+    }
+}
